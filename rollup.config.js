a3db8218d79bf72a1d5dbe47fc503cc9a1e607ee
hikaru <827394320@qq.com>
2023-10-02 20:36:00 +0800

    🐳 chore: 继续初始化项目

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,7 @@ import commonjs from "@rollup/plugin-commonjs"
 import typescript from "rollup-plugin-typescript2"
 
 module.exports = {
-	// 项目入口
 	input: "src/main.ts",
-	// 打包后的出口和设置
 	output: {
 		file: "dist/main.js",
 		format: "umd",
