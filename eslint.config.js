2976e16d72aa531eb553dba6d3a389fad4570541
hikaru <827394320@qq.com>
2024-10-18 13:50:39 +0800

    🔧 build: 更新依赖版本和eslint配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,3 @@
-import antfu from '@antfu/eslint-config'
+import wangyjhh from '@wangyjhh/eslint-config'
 
-export default antfu(
-    {
-        stylistic: {
-            indent: 4,
-        },
-        rules: {
-            'antfu/top-level-function': 'off',
-            'no-console': 'error',
-        },
-    },
-)
+export default wangyjhh()
